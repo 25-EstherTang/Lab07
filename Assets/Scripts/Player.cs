7d7dd4608d2cdcc6b1d7acd8e600a787b441d061
25-EstherTang <91871078+25-EstherTang@users.noreply.github.com>
2021-10-19 20:19:36 +0800

    Added lose condition and player not able to fly above

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -22,5 +23,23 @@ public class Player : MonoBehaviour
         GetComponent<Rigidbody>().AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
         thisAnimation.Play();
 
+        if (transform.position.y < -3)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+
+        else if (transform.position.y > 3.5f)
+        {
+            transform.position = new Vector3(transform.position.x, 4, transform.position.z);
+        }
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
