2011b114b4878564eb04bc636d5b155371588758
25-EstherTang <91871078+25-EstherTang@users.noreply.github.com>
2021-10-19 19:43:55 +0800

    Fly code added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,21 @@ public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
 
+    Rigidbody PlayerRB;
+
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        PlayerRB = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            thisAnimation.Play();
+        GetComponent<Rigidbody>().AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+        thisAnimation.Play();
+
     }
 }
